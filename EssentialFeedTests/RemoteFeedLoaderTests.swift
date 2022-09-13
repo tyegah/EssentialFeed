d1df0609f8dea0f40c94ef7fc5fd7f3a238f0f00
Tity.S <tity.septiani@gmail.com>
2022-09-13 13:22:45 +0700

    Add url as a parameter to RemoteFeedLoader because the RemoteFeedLoader does not need to know from which url it is requesting from

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,14 +9,16 @@ import XCTest
 
 class RemoteFeedLoader {
     let client: HTTPClient
-    init(client: HTTPClient) {
+    let url: URL
+    init(url: URL = URL(string: "https://a-url.com")!, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
     
     func load() {
         // We don't need to know or locate where the HTTPClient is, so we don't need the singleton
         // And it is best to use composition instead of singleton.
-        client.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: url)
     }
 }
 
@@ -37,14 +39,14 @@ class HTTPClientSpy: HTTPClient {
 class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(url: URL(string: "https://a-given-url.com")!, client: client)
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestsDataFromURL() {
         // Arrange
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(url: URL(string: "https://a-given-url.com")!, client: client)
         // Act
         sut.load()
         // Assert
