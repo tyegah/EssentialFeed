bebb7767ffc79d3afa4bf4ec8f7dbd2a5ad0af0f
Tity.S <tity.septiani@gmail.com>
2022-09-13 13:39:24 +0700

    Update RemoteFeedLoader class to be final

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -11,9 +11,9 @@ public protocol HTTPClient {
    func get(from url: URL)
 }
 
-public class RemoteFeedLoader {
-    private let client: HTTPClient
+public final class RemoteFeedLoader {
     private let url: URL
+    private let client: HTTPClient
     
     public init(url: URL, client: HTTPClient) {
         self.url = url
