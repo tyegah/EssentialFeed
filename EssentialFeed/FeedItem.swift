c249058b0c383973f0712ca33c9fbe9fad1e4bd3
Tity.S <tity.septiani@gmail.com>
2022-09-13 12:18:18 +0700

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Ty Septiani on 13/09/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
