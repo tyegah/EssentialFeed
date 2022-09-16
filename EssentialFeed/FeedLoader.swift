8e33b4491a087ad1c7808a0e816e9046b6385708
Tity.S <tity.septiani@gmail.com>
2022-09-16 21:16:05 +0700

    Change 'LoadFeedResult' typealias to enum to conform to equatable protocol

--- EssentialFeed/FeedLoader.swift
+++ EssentialFeed/FeedLoader.swift
@@ -9,8 +9,16 @@ import Foundation
 
 // We need to make this LoadFeedResult to use Generic Type Error
 // Because we are using the LoadFeedResult on the RemoteFeedLoader
-public typealias LoadFeedResult<Error:Swift.Error> = Swift.Result<[FeedItem], Error>
+//public typealias LoadFeedResult<Error:Swift.Error> = Swift.Result<[FeedItem], Error>
+
+public enum LoadFeedResult<Error: Swift.Error> {
+    case success([FeedItem])
+    case failure(Error)
+}
+
+extension LoadFeedResult: Equatable where Error:Equatable {}
 
 protocol FeedLoader {
+    associatedtype Error: Swift.Error
     func load(completion: @escaping (LoadFeedResult<Error>) -> Void)
 }
