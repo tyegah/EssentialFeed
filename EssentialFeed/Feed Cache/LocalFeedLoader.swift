e1a24260fbe9024076383d61c2d0684a0a53544a
Tity.S <tity.septiani@gmail.com>
2022-09-27 19:50:15 +0700

    Extract local members into properties

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -49,9 +49,13 @@ public final class LocalFeedLoader {
         }
     }
     
+    private var maxCacheAgeInDays:Int {
+        return 7
+    }
+    
     private func validate(_ timestamp: Date) -> Bool {
         let calendar = Calendar(identifier: .gregorian)
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
             return false
         }
         return currentDate() < maxCacheAge
