64d46b9a0d19890832d5d8ca453ab4c8ba042a2a
Tity.S <tity.septiani@gmail.com>
2022-10-03 15:47:15 +0700

    Add comments for explanations

--- EssentialFeed/Feed Cache/LocalFeedImage.swift
+++ EssentialFeed/Feed Cache/LocalFeedImage.swift
@@ -10,6 +10,9 @@ import Foundation
 // We start using this LocalFeedItem for 'FeedStore' instead of the FeedItem model to decentralized components (eliminating arrows pointing to the FeedItem model in the module diagram)
 // This object is usually called Data Transfer Object (DTO) model
 // So whenever there are changes in the FeedItem, this module will not be affected
+
+//*** The codable conformance here is leaking framework details into the Feed Cache Module
+//*** The solution is to create a framework specific model inside the CodableFeedStore
 public struct LocalFeedImage:Equatable, Codable {
     public let id: UUID
     public let description: String?
