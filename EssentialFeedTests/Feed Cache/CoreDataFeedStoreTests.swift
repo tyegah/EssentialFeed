b8d2672ed7d9d8a861f7278bb47ca71c01d1b0fc
Tity.S <tity.septiani@gmail.com>
2022-11-18 21:03:32 +0700

    CoreDataFeedStore.insert() overrides previously inserted cache values

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -41,7 +41,8 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_insert_overridesPreviouslyInsertedCacheValues() {
-        
+        let sut = makeSUT()
+        assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
     }
     
     func test_delete_deliversNoErrorOnEmptyCache() {
